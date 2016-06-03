8e1d26cb293604c6b49e08c3b59929989a3c2e66
GumSlone <gumslone@gmx.de>
2016-06-03 20:02:33 +0300

    star wars theme added

--- buzzer_player.py
+++ buzzer_player.py
@@ -16,52 +16,66 @@ notes = {
 	'B0' : 31,
 	'C1' : 33, 'CS1' : 35,
 	'D1' : 37, 'DS1' : 39,
+	'EB1' : 39,
 	'E1' : 41,
 	'F1' : 44, 'FS1' : 46,
 	'G1' : 49, 'GS1' : 52,
 	'A1' : 55, 'AS1' : 58,
+	'BB1' : 58,
 	'B1' : 62,
 	'C2' : 65, 'CS2' : 69,
 	'D2' : 73, 'DS2' : 78,
+	'EB2' : 78,
 	'E2' : 82,
 	'F2' : 87, 'FS2' : 93,
 	'G2' : 98, 'GS2' : 104,
 	'A2' : 110, 'AS2' : 117,
+	'BB2' : 123,
 	'B2' : 123,
 	'C3' : 131, 'CS3' : 139,
 	'D3' : 147, 'DS3' : 156,
+	'EB3' : 156,
 	'E3' : 165,
 	'F3' : 175, 'FS3' : 185,
 	'G3' : 196, 'GS3' : 208,
 	'A3' : 220, 'AS3' : 233,
+	'BB3' : 233,
 	'B3' : 247,
 	'C4' : 262, 'CS4' : 277,
 	'D4' : 294, 'DS4' : 311,
+	'EB4' : 311,
 	'E4' : 330,
 	'F4' : 349, 'FS4' : 370,
 	'G4' : 392, 'GS4' : 415,
 	'A4' : 440, 'AS4' : 466,
+	'BB4' : 466,
 	'B4' : 494,
 	'C5' : 523, 'CS5' : 554,
 	'D5' : 587, 'DS5' : 622,
+	'EB5' : 622,
 	'E5' : 659,
 	'F5' : 698, 'FS5' : 740,
 	'G5' : 784, 'GS5' : 831,
 	'A5' : 880, 'AS5' : 932,
+	'BB5' : 932,
 	'B5' : 988,
 	'C6' : 1047, 'CS6' : 1109,
 	'D6' : 1175, 'DS6' : 1245,
+	'EB6' : 1245,
 	'E6' : 1319,
 	'F6' : 1397, 'FS6' : 1480,
 	'G6' : 1568, 'GS6' : 1661,
 	'A6' : 1760, 'AS6' : 1865,
+	'BB6' : 1865,
 	'B6' : 1976,
 	'C7' : 2093, 'CS7' : 2217,
 	'D7' : 2349, 'DS7' : 2489,
+	'EB7' : 2489,
 	'E7' : 2637,
 	'F7' : 2794, 'FS7' : 2960,
 	'G7' : 3136, 'GS7' : 3322,
 	'A7' : 3520, 'AS7' : 3729,
+	'BB7' : 3729,
 	'B7' : 3951,
 	'C8' : 4186, 'CS8' : 4435,
 	'D8' : 4699, 'DS8' : 4978
@@ -193,6 +207,65 @@ adventure_time_tempo = [
   ]
 
 
+star_wars_melody = [ 
+					notes['G4'], notes['G4'], notes['G4'], 
+					notes['EB4'], 0, notes['BB4'], notes['G4'], 
+					notes['EB4'], 0, notes['BB4'], notes['G4'], 0,
+					
+					notes['D4'], notes['D4'], notes['D4'], 
+					notes['EB4'], 0, notes['BB3'], notes['FS3'],
+					notes['EB3'], 0, notes['BB3'], notes['G3'], 0,
+					
+					notes['G4'], 0, notes['G3'], notes['G3'], 0,
+					notes['G4'], 0, notes['FS4'], notes['F4'], 
+					notes['E4'], notes['EB4'], notes['E4'], 0,
+					notes['GS3'], notes['CS3'], 0, 
+					
+					notes['C3'], notes['B3'], notes['BB3'], notes['A3'], notes['BB3'], 0,
+					notes['EB3'], notes['FS3'], notes['EB3'], notes['FS3'], 
+					notes['BB3'], 0, notes['G3'], notes['BB3'], notes['D4'], 0,
+					
+					
+					notes['G4'], 0, notes['G3'], notes['G3'], 0,
+					notes['G4'], 0, notes['FS4'], notes['F4'], 
+					notes['E4'], notes['EB4'], notes['E4'], 0,
+					notes['GS3'], notes['CS3'], 0, 
+					
+					notes['C3'], notes['B3'], notes['BB3'], notes['A3'], notes['BB3'], 0,
+					
+					notes['EB3'], notes['FS3'], notes['EB3'],  
+					notes['BB3'], notes['G3'], notes['EB3'], 0, notes['BB3'], notes['G3'],
+					]
+
+
+star_wars_tempo = [
+					2, 2, 2, 
+					4, 8, 6, 2, 
+					4, 8, 6, 2, 8,
+					
+					2, 2, 2,
+					4, 8, 6, 2,
+					4, 8, 6, 2, 8,
+					
+					2, 16, 4, 4, 8,
+					2, 8, 4, 6,
+					6, 4, 4, 8,
+					4, 2, 8, 
+					4, 4, 6, 4, 2, 8,
+					4, 2, 4, 4, 
+					2, 8, 4, 6, 2, 8,
+					
+					2, 16, 4, 4, 8,
+					2, 8, 4, 6,
+					6, 4, 4, 8,
+					4, 2, 8, 
+					4, 4, 6, 4, 2, 8,
+					4, 2, 2, 
+					4, 2, 4, 8, 4, 2,
+					]
+
+
+
 def buzz(frequency, length):	 #create the function "buzz" and feed it the pitch and duration)
 
 	if(frequency==0):
@@ -219,14 +292,14 @@ def destroy():
 	GPIO.cleanup()				# Release resource
 	
 
-def play(melody,tempo,pace=0.800):
+def play(melody,tempo,pause,pace=0.800):
 	
 	for i in range(0, len(melody)):		# Play song
 		
 		noteDuration = pace/tempo[i]
 		buzz(melody[i],noteDuration)	# Change the frequency along the song note
 		
-		pauseBetweenNotes = noteDuration * 1.30
+		pauseBetweenNotes = noteDuration * pause
 		time.sleep(pauseBetweenNotes)
 	
 	
@@ -234,9 +307,11 @@ def play(melody,tempo,pace=0.800):
 if __name__ == '__main__':		# Program start from here
 	try:
 		setup()
-		play(melody, tempo, 0.800)
-		play(underworld_melody, underworld_tempo, 0.800)
-		play(adventure_time_melody, adventure_time_tempo, 1.5)
+		play(star_wars_melody, star_wars_tempo, 0.50, 1.000)
+		play(melody, tempo, 1.3, 0.800)
+		play(underworld_melody, underworld_tempo, 1.3, 0.800)
+		play(adventure_time_melody, adventure_time_tempo, 1.3, 1.500)
+		
 		destroy()
 	except KeyboardInterrupt:  	# When 'Ctrl+C' is pressed, the child program destroy() will be  executed.
 		destroy()
